d699a270043755006c7a71c8798975eac6cce761
elayachiamritoff <elayachiamritoff@gmail.com>
2024-02-12 20:47:15 +0100

    Add hash(key) method

--- script.rb
+++ script.rb
@@ -1,1 +1,20 @@
-#hashmap
+
+
+class HashMap
+
+    def initialize
+        @buckets = Array.new(16) { [] }
+        @size = 0
+        @load_factor = 0.75
+    end
+
+    def hash_function(key)
+        hash_code = 0
+        prim_num = 31
+        key.each_char { |char| hash_code = prim_num * hash_code + char.ord }
+
+        hash_code
+    end
+
+
+end
